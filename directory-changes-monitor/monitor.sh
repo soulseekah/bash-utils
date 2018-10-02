f9f51b9582fdb596f9a1f47be05c82f791f38943
tommieharyanto <haryanto.tommie@gmail.com>
2018-10-02 10:40:26 +0700

    Add double quotes to handle directory name with spaces, redirect stderr to null to silence error like 'Permission denied'

--- directory-changes-monitor/monitor.sh
+++ directory-changes-monitor/monitor.sh
@@ -3,9 +3,9 @@
 # A simple file changes monitor
 # usage: monitor.sh dir command
 
-A=`find $1 -printf '%t' | md5sum`;
+A=`find "$1" -printf '%t' 2>/dev/null | md5sum`;
 while true; do
-	B=`find $1 -printf '%t' | md5sum`;
+	B=`find "$1" -printf '%t' 2>/dev/null | md5sum`;
 	if [ "$A" != "$B" ]; then
 		echo "Detected change, doing: $2"
 		eval $2
